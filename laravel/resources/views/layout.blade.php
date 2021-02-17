b4014b85937c071add64befc0f17ece1e0f8cd03
hanifahnlaili <ifa.laili@gmail.com>
2021-02-17 21:39:35 +0700

    Update layout.blade.php

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -278,7 +278,7 @@ License: You must have a valid license purchased only from themeforest(the above
                             <div class="intro-y">
                                 <div class="box px-4 py-4 mb-3 flex items-center zoom-in">
                                    <!-- BEGIN: Login Form -->
-                    <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0" style="bachground: grey;">
+                    <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0" style="background: grey;">
                     <div class="my-auto mx-auto xl:ml-20 bg-white xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
                         
                         <div class="intro-x mt-2 text-gray-500 xl:hidden text-center">A few more clicks to sign in to your account. Manage all your e-commerce accounts in one place</div>
