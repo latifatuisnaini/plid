ef3ef19ad937fd7cfaa2a69ffa08413f44a45b26
deaamartya <50395247+deaamartya@users.noreply.github.com>
2021-02-18 09:12:37 +0700

    tambah text pemberitahuan

--- laravel/resources/views/maklumat.blade.php
+++ laravel/resources/views/maklumat.blade.php
@@ -5,5 +5,9 @@
     </h2>
 @endsection
 @section('content')
-    <img href="#" src="{{ asset('dist/images/maklumat.png') }}">
+<h6 class="text-sm sm:visible md:invisible lg:invisible xl:invisible 2xl:invisible text-red-500 font-bold">
+    *Geser ke kanan untuk mengunduh atau melihat dokumen.
+</h6>
+<br>
+<img href="#" src="{{ asset('dist/images/maklumat.png') }}">
 @endsection
