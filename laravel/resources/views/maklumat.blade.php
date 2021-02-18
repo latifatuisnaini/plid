7a273c1ed8f13e059e19287bcacf599458a056eb
hanifahnlaili <ifa.laili@gmail.com>
2021-02-18 10:42:05 +0700

    perubahan icon

--- laravel/resources/views/maklumat.blade.php
+++ laravel/resources/views/maklumat.blade.php
@@ -1,8 +1,11 @@
 @extends('layout')
 @section('namehalaman')
-    <h2 class="text-lg font-medium mr-auto">
+<div class="flex flex-row">
+    <i data-feather="search"></i>
+    <h2 class="text-lg font-medium mr-auto ml-3">
         MAKLUMAT
     </h2>
+</div>
 @endsection
 @section('content')
 <h6 class="text-sm hidden sm:visible md:invisible lg:invisible xl:invisible 2xl:invisible text-red-500 font-bold">
