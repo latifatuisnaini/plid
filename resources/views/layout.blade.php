f3eb47c07cf0145653b0b8aafb9bdb38ad4c558f
ivanurannisya <50392109+ivanurannisya@users.noreply.github.com>
2021-02-23 21:42:08 +0700

    edit login dan pasang env

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,7 +18,7 @@ License: You must have a valid license purchased only from themeforest(the above
         <meta name="keywords" content="admin template, Midone admin template, dashboard template, flat admin template, responsive admin template, web app">
         <meta name="author" content="LEFT4CODE">
         <title>
-            PLID PT. PAL Indonesia (Persero)
+            PPID PT. PAL Indonesia (Persero)
         </title>
         
         <!-- BEGIN: CSS Assets-->
@@ -136,7 +136,7 @@ License: You must have a valid license purchased only from themeforest(the above
                 <!-- BEGIN: Logo -->
                 <a href="" class="-intro-x hidden md:flex">
                     <img src="dist/images/logo_pt_pal_putih.png" style="width:8rem;">
-                    <span class="text-white text-lg ml-3"><span class="font-medium" style="font-size: 20px;">PUSAT LAYANAN INFORMASI DAN DOKUMENTASI</span> </span>
+                    <span class="text-white text-lg ml-3"><span class="font-medium" style="font-size: 20px;">PUSAT PELAYANAN INFORMASI DAN DOKUMENTASI</span> </span>
                 </a>
                 <!-- END: Logo -->
                 <!-- BEGIN: Breadcrumb -->
@@ -278,9 +278,8 @@ License: You must have a valid license purchased only from themeforest(the above
                             <div class="intro-y">
                                 <div class="box py-4 mb-3 flex items-center zoom-in">
                                    <!-- BEGIN: Login Form -->
-                    <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
+                    <div class="h-screen xl:h-auto flex xl:py-0 my-5 xl:my-0 px-5">
                     <div class="my-auto mx-auto xl:mx-5 bg-white xl:bg-transparent sm:py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
-                        <div class="intro-x mt-2 text-gray-500 xl:hidden text-center">A few more clicks to sign in to your account. Manage all your e-commerce accounts in one place</div>
                         <div class="intro-x mt-5">
                             Email
                             <input type="text" class="intro-x login__input input input--lg border border-gray-500 block mt-4" style="width: 100%" placeholder="Email"><br>
@@ -292,7 +291,7 @@ License: You must have a valid license purchased only from themeforest(the above
                             <button class="button button--lg w-full xl:w-32 text-white bg-theme-1 xl:mr-3 align-top">Submit</button>
                             <button class="button button--lg xl:w-32 mr-1 mb-2 bg-theme-9 text-white">Daftar</button>
                         </div>
-                        <div class="intro-x mt-10 xl:mt-24 text-gray-700 dark:text-gray-600 text-center xl:text-left">
+                        <div class="intro-x my-10 xl:mt-24 text-gray-700 dark:text-gray-600 text-center xl:text-left">
                             By signin up, you agree to our 
                             <br>
                             <a class="text-theme-1 dark:text-theme-10" href="">Terms and Conditions</a> & <a class="text-theme-1 dark:text-theme-10" href="">Privacy Policy</a> 
