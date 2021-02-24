26e5e0cefceac28e317f3867fdf32d3ed95a1d97
deaamartya <50395247+deaamartya@users.noreply.github.com>
2021-02-24 13:42:53 +0700

    tambah datatable

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -649,11 +649,9 @@ License: You must have a valid license purchased only from themeforest(the above
         <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
         <script src="https://maps.googleapis.com/maps/api/js?key=["your-google-map-api"]&libraries=places></script>
         <script src="{{asset('dist/js/app.js')}}"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <!-- END: JS Assets-->
 
         @yield('script')
-
-        
-
     </body>
 </html>
