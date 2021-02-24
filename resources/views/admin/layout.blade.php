9e4f3c2276a2202a895b61f84a863ea258a1aca2
deaamartya <50395247+deaamartya@users.noreply.github.com>
2021-02-24 13:56:09 +0700

    layout fix

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -540,7 +540,7 @@ License: You must have a valid license purchased only from themeforest(the above
                     </div>
                     <!-- END: Breadcrumb -->
                     <!-- BEGIN: Notifications -->
-                    <div class="intro-x dropdown mr-auto sm:mr-6">
+                    {{-- <div class="intro-x dropdown mr-auto sm:mr-6">
                         <div class="dropdown-toggle notification notification--bullet cursor-pointer"> <i data-feather="bell" class="notification__icon dark:text-gray-300"></i> </div>
                         <div class="notification-content pt-2 dropdown-box">
                             <div class="notification-content__box dropdown-box__content box dark:bg-dark-6">
@@ -558,81 +558,32 @@ License: You must have a valid license purchased only from themeforest(the above
                                         <div class="w-full truncate text-gray-600">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#039;s standard dummy text ever since the 1500</div>
                                     </div>
                                 </div>
-                                <div class="cursor-pointer relative flex items-center mt-5">
-                                    <div class="w-12 h-12 flex-none image-fit mr-1">
-                                        <img alt="Midone Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-11.jpg">
-                                        <div class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white"></div>
-                                    </div>
-                                    <div class="ml-2 overflow-hidden">
-                                        <div class="flex items-center">
-                                            <a href="javascript:;" class="font-medium truncate mr-5">Tom Cruise</a> 
-                                            <div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">03:20 PM</div>
-                                        </div>
-                                        <div class="w-full truncate text-gray-600">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#039;s standard dummy text ever since the 1500</div>
-                                    </div>
-                                </div>
-                                <div class="cursor-pointer relative flex items-center mt-5">
-                                    <div class="w-12 h-12 flex-none image-fit mr-1">
-                                        <img alt="Midone Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-6.jpg">
-                                        <div class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white"></div>
-                                    </div>
-                                    <div class="ml-2 overflow-hidden">
-                                        <div class="flex items-center">
-                                            <a href="javascript:;" class="font-medium truncate mr-5">John Travolta</a> 
-                                            <div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">06:05 AM</div>
-                                        </div>
-                                        <div class="w-full truncate text-gray-600">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 20</div>
-                                    </div>
-                                </div>
-                                <div class="cursor-pointer relative flex items-center mt-5">
-                                    <div class="w-12 h-12 flex-none image-fit mr-1">
-                                        <img alt="Midone Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-13.jpg">
-                                        <div class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white"></div>
-                                    </div>
-                                    <div class="ml-2 overflow-hidden">
-                                        <div class="flex items-center">
-                                            <a href="javascript:;" class="font-medium truncate mr-5">Tom Cruise</a> 
-                                            <div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">05:09 AM</div>
-                                        </div>
-                                        <div class="w-full truncate text-gray-600">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#039;s standard dummy text ever since the 1500</div>
-                                    </div>
-                                </div>
-                                <div class="cursor-pointer relative flex items-center mt-5">
-                                    <div class="w-12 h-12 flex-none image-fit mr-1">
-                                        <img alt="Midone Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-5.jpg">
-                                        <div class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white"></div>
-                                    </div>
-                                    <div class="ml-2 overflow-hidden">
-                                        <div class="flex items-center">
-                                            <a href="javascript:;" class="font-medium truncate mr-5">Kevin Spacey</a> 
-                                            <div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">01:10 PM</div>
-                                        </div>
-                                        <div class="w-full truncate text-gray-600">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomi</div>
-                                    </div>
-                                </div>
                             </div>
                         </div>
-                    </div>
+                    </div> --}}
                     <!-- END: Notifications -->
                     <!-- BEGIN: Account Menu -->
                     <div class="intro-x dropdown w-8 h-8">
                         <div class="dropdown-toggle w-8 h-8 rounded-full overflow-hidden shadow-lg image-fit zoom-in">
-                            <img alt="Midone Tailwind HTML Admin Template" src="dist/images/profile-1.jpg">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user mx-auto"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
                         </div>
                         <div class="dropdown-box w-56">
                             <div class="dropdown-box__content box bg-theme-38 dark:bg-dark-6 text-white">
                                 <div class="p-4 border-b border-theme-40 dark:border-dark-3">
-                                    <div class="font-medium">John Travolta</div>
-                                    <div class="text-xs text-theme-41 dark:text-gray-600">DevOps Engineer</div>
+                                    <div class="font-medium">{{ Auth::user()->NAMA_LENGKAP }}</div>
+                                    <div class="text-xs text-theme-41 dark:text-gray-600">{{ Auth::user()->PEKERJAAN }}</div>
                                 </div>
                                 <div class="p-2">
-                                    <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"> <i data-feather="user" class="w-4 h-4 mr-2"></i> Profile </a>
-                                    <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"> <i data-feather="edit" class="w-4 h-4 mr-2"></i> Add Account </a>
+                                    <a href="{{ route('user.show',Auth::user()->ID_USER) }}" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"> <i data-feather="user" class="w-4 h-4 mr-2"></i> Profile </a>
                                     <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"> <i data-feather="lock" class="w-4 h-4 mr-2"></i> Reset Password </a>
-                                    <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"> <i data-feather="help-circle" class="w-4 h-4 mr-2"></i> Help </a>
                                 </div>
                                 <div class="p-2 border-t border-theme-40 dark:border-dark-3">
-                                    <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"> <i data-feather="toggle-right" class="w-4 h-4 mr-2"></i> Logout </a>
+                                    <form action="{{ route('logout') }}" method="POST">
+                                        @csrf
+                                        <button class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"> 
+                                            <i data-feather="toggle-right" class="w-4 h-4 mr-2"></i> Logout 
+                                        </button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
--- routes/web.php
+++ routes/web.php
@@ -57,5 +57,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::prefix('admin')->middleware(['auth'])->group(function(){
     Route::get('/','AdminController@index');
-    Route::get('/user','UserController@index');
+    Route::resource('user','UserController');
 });
