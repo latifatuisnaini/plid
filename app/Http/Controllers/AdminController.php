4bc78faa6b37a4409dfc6aa1b92e742ab7582106
ivanurannisya <50392109+ivanurannisya@users.noreply.github.com>
2021-02-24 15:21:08 +0700

    update dashboard lagi

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Permohonan;
+use App\Models\User;
+use Illuminate\Support\Carbon;
 
 class AdminController extends Controller
 {
@@ -13,7 +15,14 @@ class AdminController extends Controller
         $permohonan_open = Permohonan::where('ID_STATUS', '1')->count('ID_PERMOHONAN');
         $permohonan_diproses = Permohonan::where('ID_STATUS', '2')->count('ID_PERMOHONAN');
         $permohonan_diterima = Permohonan::where('ID_STATUS', '3')->count('ID_PERMOHONAN');
-        return view('admin.dashboard', compact('permohonan_open', 'permohonan_diproses', 'permohonan_diterima'));
+        $konfirmasi = User::where('STATUS_KONFIRMASI', '0')->count('ID_USER');
+        $jml_permohonan_this_month  = Permohonan::whereMonth(
+            'tanggal', '=', Carbon::now()->month
+        )->count('ID_PERMOHONAN');
+        $jml_permohonan_last_month  = Permohonan::whereMonth(
+            'tanggal', '=', Carbon::now()->subMonth()->month
+        )->count('ID_PERMOHONAN');
+        return view('admin.dashboard', compact('permohonan_open', 'permohonan_diproses', 'permohonan_diterima', 'konfirmasi', 'jml_permohonan_last_month', 'jml_permohonan_this_month'));
     }
 
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -60,7 +60,7 @@
                             <div class="flex">
                                 <i data-feather="users" class="report-box__icon text-theme-11"></i> 
                             </div>
-                            <div class="text-3xl font-bold leading-8 mt-6">152.000</div>
+                            <div class="text-3xl font-bold leading-8 mt-6">{{ $konfirmasi }}</div>
                             <div class="text-base text-gray-600 mt-1">Jumlah User Menunggu Konfirmasi</div>
                         </div>
                     </div>
@@ -75,23 +75,17 @@
                 <h2 class="text-lg font-medium truncate mr-5">
                     Grafik Jumlah Permohonan Tahun 2021
                 </h2>
-                <!--
-                <div class="sm:ml-auto mt-3 sm:mt-0 relative text-gray-700 dark:text-gray-300">
-                    <i data-feather="calendar" class="w-4 h-4 z-10 absolute my-auto inset-y-0 ml-3 left-0"></i> 
-                    <input type="text" class="datepicker input w-full sm:w-56 box pl-10">
-                </div>
-                -->
             </div>
             <div class="intro-y box p-5 mt-12 sm:mt-5">
                 <div class="flex flex-col xl:flex-row xl:items-center">
                     <div class="flex">
                         <div>
-                            <div class="text-theme-20 dark:text-gray-300 text-lg xl:text-xl font-bold">$15,000</div>
+                            <div class="text-theme-20 dark:text-gray-300 text-lg xl:text-xl font-bold">{{ $jml_permohonan_this_month }}</div>
                             <div class="text-gray-600 dark:text-gray-600">This Month</div>
                         </div>
                         <div class="w-px h-12 border border-r border-dashed border-gray-300 dark:border-dark-5 mx-4 xl:mx-6"></div>
                         <div>
-                            <div class="text-gray-600 dark:text-gray-600 text-lg xl:text-xl font-medium">$10,000</div>
+                            <div class="text-gray-600 dark:text-gray-600 text-lg xl:text-xl font-medium">{{ $jml_permohonan_last_month }}</div>
                             <div class="text-gray-600 dark:text-gray-600">Last Month</div>
                         </div>
                     </div>
