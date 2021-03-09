ee0239a55c32794297af07cfceea0465650fb3eb
deaamartya <50395247+deaamartya@users.noreply.github.com>
2021-03-09 14:58:02 +0700

    edit dashboard

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -61,47 +61,86 @@
                 @endif
             </div>
             <div class="grid grid-cols-12 gap-6 mt-5">
-                    <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
-                        <div class="report-box zoom-in">
-                            <div class="box p-5">
-                                <div class="flex">
-                                    <i data-feather="file-text" class="report-box__icon text-theme-10"></i> 
-                                </div>
-                                
-                                <div class="text-base text-gray-600 mt-1">Jumlah Permohonan</div>
-                                <div class="mt-4"> 
-                                <span class="px-3 py-2 rounded bg-theme-1 text-white mr-1">6</span>
-                                </div>
+                <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                    <div class="report-box zoom-in">
+                        <div class="box p-5">
+                            <div class="flex">
+                                <i data-feather="file-text" class="report-box__icon text-theme-10"></i> 
+                            </div>
+                            <div class="text-base text-gray-600 mt-1">Jumlah Permohonan</div>
+                            <div class="mt-4"> 
+                            <span class="px-3 py-2 rounded bg-theme-1 text-white mr-1">6</span>
                             </div>
                         </div>
                     </div>
-                    @if(count($list_permohonan) > 0)
-                    <div class="col-span-12 xl:col-span-4">
-                        <div class="intro-y flex items-center h-10">
-                            <h2 class="text-lg font-medium truncate mr-5">
-                                Status Permohonan
-                            </h2>
-                        </div>
-                        <div class="mt-5">
-                            
-                            @foreach($list_permohonan as $list)
-                            <div class="intro-y">
-                                <div class="box px-4 py-4 mb-3 flex items-center zoom-in">
-                                    <div class="w-10 h-10 flex-none image-fit rounded-md overflow-hidden">
-                                    <i data-feather="file-text" class="report-box__icon text-theme-10"></i> 
-                                    </div>
-                                    <div class="ml-auto mr-auto">
-                                        <div class="font-medium">{{ $list->DOKUMEN_PERMOHONAN }}</div>
-                                        <div class="text-gray-600 text-xs">{{ $list->tgl_permohonan }}</div>
-                                    </div>
-                                    <div class="py-1 px-2 rounded-full text-xs bg-theme-9 text-white cursor-pointer font-medium">{{ $list->STATUS }}</div>
-                                </div>
+                </div>
+                <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                    <div class="report-box zoom-in">
+                        <div class="box p-5">
+                            <div class="flex">
+                                <i data-feather="file-text" class="report-box__icon text-theme-10"></i> 
                             </div>
-                            @endforeach                               
-                            <a href="" class="intro-y w-full block text-center rounded-md py-4 border border-dotted border-theme-15 dark:border-dark-5 text-theme-16 dark:text-gray-600">View More</a>
-                            
+                            <div class="text-base text-gray-600 mt-1">Jumlah Permohonan</div>
+                            <div class="mt-4"> 
+                            <span class="px-3 py-2 rounded bg-theme-1 text-white mr-1">6</span>
+                            </div>
                         </div>
-                                  </div>
+                    </div>
+                </div>
+                <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                    <div class="report-box zoom-in">
+                        <div class="box p-5">
+                            <div class="flex">
+                                <i data-feather="file-text" class="report-box__icon text-theme-10"></i> 
+                            </div>
+                            <div class="text-base text-gray-600 mt-1">Jumlah Permohonan</div>
+                            <div class="mt-4"> 
+                            <span class="px-3 py-2 rounded bg-theme-1 text-white mr-1">6</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                    <div class="report-box zoom-in">
+                        <div class="box p-5">
+                            <div class="flex">
+                                <i data-feather="file-text" class="report-box__icon text-theme-10"></i> 
+                            </div>
+                            <div class="text-base text-gray-600 mt-1">Jumlah Permohonan</div>
+                            <div class="mt-4"> 
+                            <span class="px-3 py-2 rounded bg-theme-1 text-white mr-1">6</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="grid grid-cols-12 gap-6 mt-5">
+                @if(count($list_permohonan) > 0)
+                <div class="col-span-12 xl:col-span-4">
+                    <div class="intro-y flex items-center h-10">
+                        <h2 class="text-lg font-medium truncate mr-5">
+                            Status Permohonan
+                        </h2>
+                    </div>
+                    <div class="mt-5">
+                        
+                        @foreach($list_permohonan as $list)
+                        <div class="intro-y">
+                            <div class="box px-4 py-4 mb-3 flex items-center zoom-in">
+                                <div class="mr-auto">
+                                    <div class="font-medium">{{ $list->DOKUMEN_PERMOHONAN }}</div>
+                                    <div class="text-gray-600 text-xs">{{ $list->tgl_permohonan }}</div>
+                                </div>
+                                <div class="py-1 px-2 rounded-full text-xs bg-theme-9 text-white cursor-pointer font-medium">{{ $list->STATUS }}</div>
+                            </div>
+                        </div>
+                        @endforeach                               
+                        <a href="" class="intro-y w-full block text-center rounded-md py-4 border border-dotted border-theme-15 dark:border-dark-5 text-theme-16 dark:text-gray-600">View More</a>
+                        
+                    </div>
+                </div>
+                @endif
+            </div>
     </div>
 </div>
 
