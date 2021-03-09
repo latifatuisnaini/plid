634a9f6116832358f9a5ce5a2fa9be316aea6fc7
deaamartya <50395247+deaamartya@users.noreply.github.com>
2021-03-09 09:20:51 +0700

    dashboard

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -2,21 +2,21 @@
 @section('content')
 <div class="grid grid-cols-12 gap-6">
     <div class="col-span-12 xxl:col-span-9 grid grid-cols-12 gap-6">
-    <div class="col-span-12 mt-8">
+        <div class="col-span-12 mt-8">
             <div class="intro-y flex items-center h-10">
                 <h2 class="text-lg font-medium truncate mr-5">
                     User Dashboard
                 </h2>
                 <a href="" class="ml-auto flex text-theme-1 dark:text-theme-10"> <i data-feather="refresh-ccw" class="w-4 h-4 mr-3"></i> Reload Data </a>
             </div>
-    <div class="grid grid-cols-12 gap-6 mt-5">
+            <div class="grid grid-cols-12 gap-6 mt-5">
                 <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
                     <div class="report-box zoom-in">
                         <div class="box p-5">
                             <div class="flex">
                                 <i data-feather="file-text" class="report-box__icon text-theme-10"></i> 
                             </div>
-                           
+                            
                             <div class="text-base text-gray-600 mt-1">Status User</div>
                             <div class="mt-4"> 
                             <span class="px-3 py-2 rounded-full bg-theme-1 text-white mr-1">Status : {{ $verified }} </span>
@@ -30,7 +30,7 @@
                             <div class="flex">
                                 <i data-feather="file-text" class="report-box__icon text-theme-12"></i> 
                             </div>
-                           
+                            
                             <div class="text-base text-gray-600 mt-1">Jumlah Permohonan</div>
                             <div class="mt-4">
                             <span class="px-3 py-2 rounded-full bg-theme-12 text-white mr-1">Status : Sedang di Proses</span> 
@@ -44,7 +44,7 @@
                             <div class="flex">
                                 <i data-feather="file-text" class="report-box__icon text-theme-9"></i> 
                             </div>
-                           
+                            
                             <div class="text-base text-gray-600 mt-1">Jumlah Permohonan</div>
                             <div class="mt-4">
                             <span class="px-3 py-2 rounded-full bg-theme-9 text-white mr-1">Status : Diterima</span> 
@@ -53,7 +53,7 @@
                     </div>
                 </div>
             </div>
-    </div>
+        </div>
         
         </div>
     </div>
