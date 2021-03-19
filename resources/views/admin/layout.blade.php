2a451824f8f1a675d70ce49a803ca26e6c2d50b6
deaamartya <50395247+deaamartya@users.noreply.github.com>
2021-03-19 09:25:06 +0700

    Update layout.blade.php

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -51,6 +51,21 @@ License: You must have a valid license purchased only from themeforest(the above
                         <div class="menu__title"> User </div>
                     </a>
                 </li>
+
+                <li>
+                    <a href="{{url('/admin/verif')}}" class="menu">
+                        <div class="menu__icon"> <i data-feather="inbox"></i> </div>
+                        <div class="menu__title"> Verifikasi Dokumen User </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{url('/admin/permohonan-open')}}" class="menu">
+                        <div class="menu__icon"> <i data-feather="inbox"></i> </div>
+                        <div class="menu__title">Permohonan : Open</div>
+                    </a>
+                </li>
+                
             </ul>
         </div>
         <!-- END: Mobile Menu -->
@@ -85,6 +100,14 @@ License: You must have a valid license purchased only from themeforest(the above
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-square"><polyline points="9 11 12 14 22 4"></polyline><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path></svg>
                             </div>
                             <div class="side-menu__title"> Verifikasi Dokumen User </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{url('/admin/permohonan-open')}}" class="side-menu @if(request() -> segment(1) == 'admin' && request()->segment(2) == 'permohonan-open') side-menu--active @endif">
+                            <div class="side-menu__icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-square"><polyline points="9 11 12 14 22 4"></polyline><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path></svg>
+                            </div>
+                            <div class="side-menu__title"> Permohonan : Open </div>
                         </a>
                     </li>
                 </ul>
