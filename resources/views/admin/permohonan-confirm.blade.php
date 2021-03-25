7eae4b7f9179c70948df1024e7c2734576a073ff
hanifahnlaili <ifa.laili@gmail.com>
2021-03-25 15:01:08 +0700

    print 1

--- resources/views/admin/permohonan-confirm.blade.php
+++ resources/views/admin/permohonan-confirm.blade.php
@@ -4,66 +4,72 @@
 <link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
 <!--Responsive Extension Datatables CSS-->
 <link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" rel="stylesheet">
+<script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
         
 <style>
-/*Form fields*/
-.dataTables_wrapper select,
-.dataTables_wrapper .dataTables_filter input {
-    color: #4a5568; 			/*text-gray-700*/
-    padding-left: 1rem; 		/*pl-4*/
-    padding-right: 1rem; 		/*pl-4*/
-    padding-top: .5rem; 		/*pl-2*/
-    padding-bottom: .5rem; 		/*pl-2*/
-    line-height: 1.25; 			/*leading-tight*/
-    border-width: 2px; 			/*border-2*/
-    border-radius: .25rem; 		
-    border-color: #edf2f7; 		/*border-gray-200*/
-    background-color: #edf2f7; 	/*bg-gray-200*/
-}
+    /*Form fields*/
+    .dataTables_wrapper select,
+    .dataTables_wrapper .dataTables_filter input {
+        color: #4a5568; 			/*text-gray-700*/
+        padding-left: 1rem; 		/*pl-4*/
+        padding-right: 1rem; 		/*pl-4*/
+        padding-top: .5rem; 		/*pl-2*/
+        padding-bottom: .5rem; 		/*pl-2*/
+        line-height: 1.25; 			/*leading-tight*/
+        border-width: 2px; 			/*border-2*/
+        border-radius: .25rem; 		
+        border-color: #edf2f7; 		/*border-gray-200*/
+        background-color: #edf2f7; 	/*bg-gray-200*/
+    }
 
-/*Row Hover*/
-table.dataTable.hover tbody tr:hover, table.dataTable.display tbody tr:hover {
-    background-color: #ebf4ff;	/*bg-indigo-100*/
-}
+    /*Row Hover*/
+    table.dataTable.hover tbody tr:hover, table.dataTable.display tbody tr:hover {
+        background-color: #ebf4ff;	/*bg-indigo-100*/
+    }
 
-/*Pagination Buttons*/
-.dataTables_wrapper .dataTables_paginate .paginate_button		{
-    font-weight: 700;				/*font-bold*/
-    border-radius: .25rem;			/*rounded*/
-    border: 1px solid transparent;	/*border border-transparent*/
-}
+    /*Pagination Buttons*/
+    .dataTables_wrapper .dataTables_paginate .paginate_button		{
+        font-weight: 700;				/*font-bold*/
+        border-radius: .25rem;			/*rounded*/
+        border: 1px solid transparent;	/*border border-transparent*/
+    }
 
-/*Pagination Buttons - Current selected */
-.dataTables_wrapper .dataTables_paginate .paginate_button.current	{
-    color: #fff !important;				/*text-white*/
-    box-shadow: 0 1px 3px 0 rgba(0,0,0,.1), 0 1px 2px 0 rgba(0,0,0,.06); 	/*shadow*/
-    font-weight: 700;					/*font-bold*/
-    border-radius: .25rem;				/*rounded*/
-    background: #667eea !important;		/*bg-indigo-500*/
-    border: 1px solid transparent;		/*border border-transparent*/
-}
+    /*Pagination Buttons - Current selected */
+    .dataTables_wrapper .dataTables_paginate .paginate_button.current	{
+        color: #fff !important;				/*text-white*/
+        box-shadow: 0 1px 3px 0 rgba(0,0,0,.1), 0 1px 2px 0 rgba(0,0,0,.06); 	/*shadow*/
+        font-weight: 700;					/*font-bold*/
+        border-radius: .25rem;				/*rounded*/
+        background: #667eea !important;		/*bg-indigo-500*/
+        border: 1px solid transparent;		/*border border-transparent*/
+    }
 
-/*Pagination Buttons - Hover */
-.dataTables_wrapper .dataTables_paginate .paginate_button:hover		{
-    color: #fff !important;				/*text-white*/
-    box-shadow: 0 1px 3px 0 rgba(0,0,0,.1), 0 1px 2px 0 rgba(0,0,0,.06);	 /*shadow*/
-    font-weight: 700;					/*font-bold*/
-    border-radius: .25rem;				/*rounded*/
-    background: #667eea !important;		/*bg-indigo-500*/
-    border: 1px solid transparent;		/*border border-transparent*/
-}
+    /*Pagination Buttons - Hover */
+    .dataTables_wrapper .dataTables_paginate .paginate_button:hover		{
+        color: #fff !important;				/*text-white*/
+        box-shadow: 0 1px 3px 0 rgba(0,0,0,.1), 0 1px 2px 0 rgba(0,0,0,.06);	 /*shadow*/
+        font-weight: 700;					/*font-bold*/
+        border-radius: .25rem;				/*rounded*/
+        background: #667eea !important;		/*bg-indigo-500*/
+        border: 1px solid transparent;		/*border border-transparent*/
+    }
 
-/*Add padding to bottom border */
-table.dataTable.no-footer {
-    border-bottom: 1px solid #e2e8f0;	/*border-b-1 border-gray-300*/
-    margin-top: 0.75em;
-    margin-bottom: 0.75em;
-}
+    /*Add padding to bottom border */
+    table.dataTable.no-footer {
+        border-bottom: 1px solid #e2e8f0;	/*border-b-1 border-gray-300*/
+        margin-top: 0.75em;
+        margin-bottom: 0.75em;
+    }
 
-/*Change colour of responsive icon*/
-table.dataTable.dtr-inline.collapsed>tbody>tr>td:first-child:before, table.dataTable.dtr-inline.collapsed>tbody>tr>th:first-child:before {
-    background-color: #667eea !important; /*bg-indigo-500*/
-}
+    /*Change colour of responsive icon*/
+    table.dataTable.dtr-inline.collapsed>tbody>tr>td:first-child:before, table.dataTable.dtr-inline.collapsed>tbody>tr>th:first-child:before {
+        background-color: #667eea !important; /*bg-indigo-500*/
+    }
 
 </style>
 @endsection
@@ -82,25 +88,26 @@ table.dataTable.dtr-inline.collapsed>tbody>tr>td:first-child:before, table.dataT
 
     <div class="intro-y block sm:flex items-center h-10">
         <h2 class="text-lg font-medium truncate mr-5">
-            Weekly Top Products
+            Print Tabel Permohonan yang Sudah Dikonfirmasi
         </h2>
         <div class="flex items-center sm:ml-auto mt-3 sm:mt-0">
             <!-- <button class="button box flex items-center shadow-md bg-gray-200 text-gray-700"> <i data-feather="file-text" class="hidden sm:block w-4 h-4 mr-2"></i> Export to Excel </button> -->
-            <button class="ml-3 button box flex items-center shadow-md bg-gray-200 text-gray-700"> <i data-feather="file-text" class="hidden sm:block w-4 h-4 mr-2"></i> Export to PDF </button>
+            <button class="ml-3 button box flex items-center shadow-md bg-gray-200 text-gray-700" id="print"> <i data-feather="printer" class="hidden sm:block w-4 h-4 mr-2"></i> Print PDF </button>
         </div>
-    </div>  <br>
+    </div>  
+    <br>
 
     <!--Card-->
     <div class="p-8 mt-6 lg:mt-0 rounded shadow bg-white">
     
-        <table id="example" class="stripe hover display cell-border" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
+        <table id="view" class="stripe hover display cell-border" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
             <thead>
                 <tr>
-                    <th data-priority="3">Tanggal</th>
-                    <th data-priority="1">Nama Dokumen</th>
-                    <th data-priority="2">Keterangan</th>
-                    <th data-priority="2">Status</th>
-                    <th data-priority="6">Aksi</th>
+                    <th data-priority="1">Tanggal</th>
+                    <th data-priority="2">Nama Dokumen</th>
+                    <th data-priority="3">Keterangan</th>
+                    <th data-priority="4">Status</th>
+                    <th data-priority="5">Aksi</th>
                 </tr>
             </thead>
             <tbody>
@@ -125,6 +132,13 @@ table.dataTable.dtr-inline.collapsed>tbody>tr>td:first-child:before, table.dataT
                             <a data-toggle="modal" data-target="#detail_{{ $p->ID_PERMOHONAN }}">
                                 <span class="w-5 h-5 flex items-center justify-center">
                                     <i data-feather="more-horizontal" class="w-4 h-4 "></i>
+                                </span>
+                            </a>
+                        </button>
+                        <button href="javascript:;" title="Print Permohonan" type="button" class="tooltip button px-2 mr-1 mb-2 bg-blue-300 dark:text-gray-300">
+                            <a data-target="#print_{{ $p->ID_PERMOHONAN }}">
+                                <span class="w-5 h-5 flex items-center justify-center">
+                                    <i data-feather="printer" class="w-4 h-4"></i>
                                 </span>
                             </a>
                         </button>
@@ -208,12 +222,36 @@ table.dataTable.dtr-inline.collapsed>tbody>tr>td:first-child:before, table.dataT
 <script>
 $(document).ready(function() {
 
-    var table = $('#example').DataTable( {
+    var table = $('#view').DataTable( {
             responsive: true
         } )
         .columns.adjust()
         .responsive.recalc();
 
 });
+
+$(document).ready(function() {
+    $('#print').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: 'pdfHtml5',
+                download: 'open'
+            }
+        ]
+    } );
+} );
+
+$(document).ready(function() {
+    $('#print_{{ $p->ID_PERMOHONAN }}').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: 'pdfHtml5',
+                download: 'open'
+            }
+        ]
+    } );
+} );
 </script>
 @endsection
