8bddf07c2da63118f66bf3fbd8719b34478800de
hanifahnlaili <ifa.laili@gmail.com>
2021-03-31 13:30:38 +0700

    tambahan ifa dan print

--- resources/views/admin/cetak-permohonan-confirm.blade.php
+++ resources/views/admin/cetak-permohonan-confirm.blade.php
@@ -2,26 +2,31 @@
 <html>
 <head>
 	<title>Permohonan User yang Sudah Dikonfirmasi</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="{{$_SERVER['DOCUMENT_ROOT'] . '/public/dist/css/app.css'}}" >
 </head>
 <body>
 	<style type="text/css">
         h5 {color: darkslateblue; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;}
-		table tr td,
-		table tr th{
-			font-size: 9pt;
-		}
+        table{
+            border: 1pt black;
+        }
+
+	    th{
+            color: black;
+            font-size: 10pt;
+        } 
+        
 	</style>
     
     <h5>Daftar Permohonan User yang Sudah Dikonfirmasi</h4>
     <br>
-    <table id="view" class="stripe hover display cell-border" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
+    <table id="view" class=" stripe hover display cell-border" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
         <thead>
             <tr>
-                <th data-priority="3">Tanggal</th>
-                <th data-priority="1" width="20%">Nama Dokumen</th>
+                <th data-priority="3" width="12%">Tanggal</th>
+                <th data-priority="1" width="12%">Nama Dokumen</th>
                 <th data-priority="2" width="30%">Keterangan Dokumen</th>
-                <th data-priority="2">Status</th>
+                <th data-priority="2" width="10%">Status</th>
             </tr>
         </thead>
         <tbody>
@@ -47,26 +52,4 @@
     </table>
  
 </body>
-
-<!--Datatables -->
-<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
- 
-<script>
-$(document).ready(function() {
-
-    var table = $('#view').DataTable( {
-        responsive: true,
-    } )
-    .columns.adjust()
-    .responsive.recalc();
-});
-
-</script>
 </html>
--- resources/views/admin/cetak-permohonan-open.blade.php
+++ resources/views/admin/cetak-permohonan-open.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>Permohonan User yang Belum Dikonfirmasi</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="{{$_SERVER['DOCUMENT_ROOT'] . '/public/dist/css/app.css'}}" >
 </head>
 <body>
 	<style type="text/css">
@@ -38,25 +38,4 @@
  
 </body>
 
-<!--Datatables -->
-<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
- 
-<script>
-$(document).ready(function() {
-
-    var table = $('#view').DataTable( {
-        responsive: true,
-    } )
-    .columns.adjust()
-    .responsive.recalc();
-});
-
-</script>
 </html>
