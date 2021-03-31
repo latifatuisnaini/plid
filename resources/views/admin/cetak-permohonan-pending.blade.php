b7173b3199a5d2e20b81d6df4cf5350d2073a6ac
hanifahnlaili <ifa.laili@gmail.com>
2021-03-31 15:22:35 +0700

    print all tinggal benerin tulisan ptpal

--- resources/views/admin/cetak-permohonan-pending.blade.php
+++ resources/views/admin/cetak-permohonan-pending.blade.php
@@ -3,18 +3,35 @@
 <head>
 	<title>Permohonan User yang Sedang Diproses</title>
 	<link rel="stylesheet" href="{{$_SERVER['DOCUMENT_ROOT'] . '/public/dist/css/app.css'}}" >
+    <link rel="stylesheet" href="{{$_SERVER['DOCUMENT_ROOT'] . '/public/dist/images/logo-pal.png'}}" >
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
 	<style type="text/css">
-        h5 {color: darkslateblue; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;}
-		table tr td,
-		table tr th{
-			font-size: 9pt;
-		}
+        h3 {
+            color: black; 
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            text-align: center;
+        }
+        table,tr,th, td{
+            border: 1pt solid black;
+            border-collapse: collapse;
+        }
+        th, td{
+            padding: 5px;
+            font-size: 9pt;
+        }
+	    th{
+            color: black;
+            font-size: 10pt;
+        } 
 	</style>
     
-    <h5>Daftar Permohonan User yang Sedang Diproses</h4>
-    <br>
+    <img src="{{ public_path('dist/images/logo-pal.png') }}" style="width:8rem;"> 
+    <span style="font-size: 20px; color: #03428b; font-family: 'Lato', sans-serif;  display: inline-block; margin-left: 10px; font-weight: 400;">PT. PAL Indonesia (Persero)</span>
+
+    <h3>Daftar Permohonan User yang Sedang Diproses</h3>
+
     <table id="example" class="stripe hover display cell-border" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
         <thead>
             <tr>
