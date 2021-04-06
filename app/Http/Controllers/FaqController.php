46ea029402770c003f58da8e125d97ff9cc8e026
ivanurannisya <50392109+ivanurannisya@users.noreply.github.com>
2021-04-06 10:45:30 +0700

    update faq

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Faq;
+use App\Models\Faq;
 use App\Models\Permohonan;
 
 class FaqController extends Controller
